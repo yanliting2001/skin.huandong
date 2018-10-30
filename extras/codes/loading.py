a06a8a9c96b5367a6a90a23006b44f6aee807187
yugimaster <wei.yuan@zenithnine.com>
2018-10-30 11:55:34 +0800

    rework loading page

--- extras/codes/loading.py
+++ extras/codes/loading.py
@@ -8,12 +8,20 @@ import random
 HOME = xbmcgui.Window(10000)
 HOME.setProperty("LoadingPercent", "1%")
 control_progress = HOME.getControl(9001)
-count = 0
+loading_count = 0
+count = 3
 while (count < 100):
     randomInt = random.randint(1, 5)
     count += randomInt
+    loading_count += 1
     if count > 100:
         count = 100
+    if loading_count % 3 == 1:
+        HOME.setProperty("LoadingPoint", ".")
+    elif loading_count % 3 == 2:
+        HOME.setProperty("LoadingPoint", "..")
+    elif loading_count % 3 == 0:
+        HOME.setProperty("LoadingPoint", "...")
     loadingPercent = str(count) + "%"
     control_progress.setPercent(float(count))
     HOME.setProperty("LoadingPercent", loadingPercent)
